93ab453e540dc5a93e133718f71d2ad4513c87ca
Gordon Stewart <gstew5@gmail.com>
2018-05-14 14:36:44 -0400

    Begin reorg: In particular: isolate Gibbs and Pinsker lemmas in an axioms.v file, to be proved at some point (soon). There are a couple remaining holes, all to do with a single inequality in expfacts.v. learning.v (which has an admit) has yet to be updated wrt. lemmas recently proved in chernoff.v.
    A couple other notes: We should investigate
    Reynald Affeldt's development in information theory:
    https://staff.aist.go.jp/reynald.affeldt/shannon/
    He's already proved Pinsker's lemma, and his formalization
    of (finite) prob. distributions seems compatible with our own
    (in particular, he formalizes distributions as pmfs to Coq standard
    library positive R, which is quite close to the second formalization
    of finite distributions in our dist.v).

---
+++ axioms.v
@@ -0,0 +1,31 @@
+Set Implicit Arguments.
+Unset Strict Implicit.
+
+Require Import mathcomp.ssreflect.ssreflect.
+From mathcomp Require Import all_ssreflect.
+From mathcomp Require Import all_algebra.
+
+Import GRing.Theory Num.Def Num.Theory.
+
+Require Import QArith Reals Rpower Ranalysis Fourier Lra.
+
+Require Import dist.
+
+(** The following lemmas (stated here as axioms) are well 
+    known but should nevertheless be proved. Until proved,
+    any paper that uses/references results from OUVerT.axioms
+    should state explicitly that the formal claims depend upon
+    the following assumptions: 
+
+    NOTE: We should see whether the results from Affeldt et al's: 
+    https://staff.aist.go.jp/reynald.affeldt/shannon/ 
+    can be imported here to prove [pinsker] and related theorems
+    in information theory. *)
+
+Axiom pinsker_Bernoulli : 
+  forall p q : R, 
+  sqrt (RE_Bernoulli p q / 2) >= TV_Bernoulli p q.
+
+Axiom gibbs_Bernoulli : 
+  forall p q : R, 0 <= RE_Bernoulli p q.
+
