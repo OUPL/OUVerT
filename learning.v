550c76f1af4294bf875acc242aff718b767f08d0
Gordon Stewart <gstew5@gmail.com>
2018-06-27 17:38:03 -0400

    Struggling with epsilon

--- learning.v
+++ learning.v
@@ -5,10 +5,12 @@ Require Import mathcomp.ssreflect.ssreflect.
 From mathcomp Require Import all_ssreflect.
 From mathcomp Require Import all_algebra.
 
+Require Import FunctionalExtensionality.
+
 Import GRing.Theory Num.Def Num.Theory.
 Require Import QArith Reals Rpower Ranalysis Fourier.
 
-Require Import bigops numerics expfacts dist.
+Require Import bigops numerics expfacts dist chernoff.
 
 Section learning.
   Variables A B : finType.
@@ -22,7 +24,7 @@ Section learning.
 
   Variable m : nat. (*The number of training samples*)
   Variable m_gt0 : (0 < m)%nat.  
-  Definition mR := INR m.
+  Notation mR := (INR m).
   Definition i0 : 'I_m := Ordinal m_gt0.  
 
   (** Training sets *)
@@ -43,26 +45,45 @@ Section learning.
   Variable identical : forall h : Hyp, identically_distributed d (X h).
   Variable independent : forall h : Hyp, mutual_independence d (X h).
 
-  Lemma chernoff_bound_h (h : Hyp) (eps : R) (eps_range : 0 < eps < 1 - expErr h) :
-    phat_ge_q d m_gt0 (X h) eps <= exp (-2%R * eps^2 * mR).
+  Lemma chernoff_bound_h
+        (h : Hyp) (eps : R) (eps_gt0 : 0 < eps)
+        (eps_range1 : eps < 1 - expErr h)
+        (eps_range2 : eps < expErr h) :
+    probOfR
+      (prodR (fun _ : 'I_m => d))
+      (fun T : training_set => Rle_lt_dec eps (Rabs (expErr h - empErr T h))) <=
+    2 * exp (-2%R * eps^2 * mR).
   Proof.
-    apply: chernoff2 => //.
-    { move => i x; case: x => a b; rewrite /X.
-      case: (h a == b)%B; split; fourier. }
-    { by apply: (expErr_nontrivial h). }
-    { by case: eps_range. }
-    by case: eps_range.
+    have H1: expErr h = p_exp d m_gt0 (X h) by [].
+    have H2:
+      probOfR (T:=finfun_of_finType (ordinal_finType m) (prod_finType A B))
+        (prodR (T:=prod_finType A B) (fun _ : 'I_m => d))
+        (fun T : training_set => Rle_lt_dec eps (Rabs (p_exp (T:=prod_finType A B) d m_gt0 (X h) - empErr T h))) = 
+      probOfR (T:=finfun_of_finType (ordinal_finType m) (prod_finType A B))
+        (prodR (T:=prod_finType A B) (fun _ : 'I_m => d))
+        (fun T => Rle_lt_dec eps (Rabs (p_exp (T:=prod_finType A B) d m_gt0 (X h) - p_hat (X h) T))).
+    { rewrite /probOfR; apply: big_sum_ext => //=; apply eq_in_filter => T Hin.
+      have ->: empErr T h = p_hat (X h) T.
+      { rewrite /p_hat /empErr; rewrite Rmult_comm //. }
+      by []. }
+    rewrite H1 H2.
+    apply: chernoff => //; try fourier.
+    { move => i x; rewrite /X; case: x; split; case: (h a == b)%B; fourier. }
+    { move: H1; rewrite /p_exp => <- //. }
+    move: H1; rewrite /p_exp => <- //.
   Qed.
-  
-  Lemma chernoff_bound_aux (eps : R) (eps_range : 0 < eps) :
-    probOfR (prodR (fun _ => d))
+
+  Lemma chernoff_bound (eps : R) (eps_gt0 : 0 < eps) :
+    probOfR (prodR (fun _ : 'I_m => d))
             [pred T:training_set
             | [exists i : 'I_#|Hyp|,
-                let: h := enum_val i in Rle_lt_dec (expErr h + eps) (empErr T h)]]
+                 let: h := enum_val i 
+                 in Rle_lt_dec eps (Rabs (expErr h - empErr T h))]]
     <= 2 * INR #|Hyp| * exp (-2%R * eps^2 * mR).
   Proof.
     set (P := fun i:'I_#|Hyp| => 
-           [pred T : training_set | let: h := enum_val i in Rle_lt_dec (expErr h + eps) (empErr T h)]).
+         [pred T : training_set |
+         let: h := enum_val i in Rle_lt_dec eps (Rabs (expErr h - empErr T h))]).
     change (probOfR (prodR (fun _ => d)) [pred T:training_set | [exists i : 'I_#|Hyp|, P i T]] 
             <= 2 * INR #|Hyp| * exp (-2%R * eps^2 * mR)).
     apply: Rle_trans; [apply: union_bound|].
@@ -74,7 +95,9 @@ Section learning.
          (prodR (T:=prod_finType A B) (fun _ : 'I_m => d)) [eta P i]
     <= \big[Rplus/0]_(i in 'I_#|Hyp|) (2 * exp (-2%R * eps^2 * mR)).
     { rewrite -2!big_sum_sumP; apply big_sum_le => c Hin.
-      admit. }
+      apply chernoff_bound_h => //.
+      admit. (*WTF? Same weird epsilon assumption.*)
+      admit. (*WTF? Same weird epsilon assumption.*) }
     apply: Rle_trans; first by apply: Hle.
     rewrite big_const card_ord; elim: #|Hyp|.
     { rewrite !Rmult_0_l; apply: Rle_refl. }
@@ -85,7 +108,7 @@ Section learning.
     { rewrite S_INR Rmult_assoc Rmult_plus_distr_r Rmult_1_l Rplus_comm; f_equal.
       by rewrite -Rmult_assoc. }
     apply: Rplus_le_compat_l => //.
-  Qed.    
+  Admitted.
 End learning.  
   
   
